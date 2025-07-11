d1c80a6ddc6ff94c1cba9d1532f81c7eb105d563
Abhishek <hopesaliveabhi2003@gmail.com>
2025-07-11 16:57:43 +0530

    Array

---
+++ Arrays/basicsOfArr01.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class basicsOfArr01 {
+    public static void main(String[]args){
+       
+        // int[] nums = new int[4];
+        // nums[0] = 5;
+        // nums[1] = 4;
+        // nums[2] = 3;
+        // nums[3] = 5;
+
+        String[] names = {"Navin","Ram","Shyam"};
+        for (int i = 0; i <= 2; i++) {
+            System.out.println(names[i]);
+        }
+    }
+}
