5c78e58462bc40c85a61c89143e380f909903cdf
Abhishek <hopesaliveabhi2003@gmail.com>
2025-07-21 17:25:36 +0530

    methods

---
+++ string/methodsOfString04.java
@@ -0,0 +1,17 @@
+package string;
+
+public class methodsOfString04 {
+    public static void main(String[] args){
+        String str = "PW Java Skills";
+       System.out.println(str);
+       System.out.println(str.toUpperCase());
+       System.out.println(str.toLowerCase());
+       System.out.println(str.length());
+       System.out.println(str.charAt(3));
+       System.out.println(str.substring(3,8));
+       // int length = str.length();
+        System.out.println(str.indexOf("S"));
+        System.out.println(str.lastIndexOf("S"));
+
+    }
+}
