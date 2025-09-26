17e30f14a17d49803ee798bbe2e25c160abd5a20
Abhishek <hopesaliveabhi2003@gmail.com>
2025-09-26 19:45:04 +0530

    Mutable strings

---
+++ string/mutableStrings/mutableStr.java
@@ -0,0 +1,13 @@
+package string.mutableStrings;
+
+public class mutableStr {
+    public static void main(String[] args){
+        // StringBuffer sb = new StringBuffer("Virat");
+        StringBuilder sb = new StringBuilder("Virat");
+        System.out.println(sb);
+
+        //StringBuffer sb = " "; => Invalid
+        sb.append("Kohli");
+        System.out.println(sb);
+    }
+}
