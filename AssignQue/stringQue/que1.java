f7dda5fc62e1e0c97753a76bce1ad7b0536b0a46
Abhishek <hopesaliveabhi2003@gmail.com>
2025-09-26 19:07:15 +0530

    modified

---
+++ AssignQue/stringQue/que1.java
@@ -0,0 +1,9 @@
+package AssignQue.stringQue;
+
+public class que1 {
+    public static void main(String[] args){
+        String s = "SACHIN TENDULKAR";
+        int len = s.trim().length();
+        System.out.println(len);
+    }
+}
