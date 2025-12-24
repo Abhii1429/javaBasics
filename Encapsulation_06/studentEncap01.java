b86aec02d7e7c775ffcde4180b4aa01882e5c492
Abhishek <hopesaliveabhi2003@gmail.com>
2025-12-24 17:59:58 +0530

    "private" keyword

--- Encapsulation_06/studentEncap01.java
+++ Encapsulation_06/studentEncap01.java
@@ -7,12 +7,12 @@ class Student{
     // private can be accessed only within the same class
 
     public void setData(){
-        age = 18;
+        age = 18; 
         name = "Abhishek";
     }
 
     public void show(){
-        System.out.println(name +" "+age);
+        System.out.println(name +" "+age); // accessing private data members within the class
     }
 }
 
@@ -21,10 +21,16 @@ public class studentEncap01 {
         Student obj = new Student();
         // obj.age = 18; // we can directly access the data members of class Student 
         // to avoid this we use encapsulation(make data members as private)
+        
         // obj.name = "Rahul";
         //after making data members "private" we can't access them directly
         
         obj.setData(); // using public method to set the data
-        obj.show();
+        obj.show(); // calling show() method to display the data
+        
+        // this is how encapsulation works, we can set the data using public method and 
+        // access the data using another public method
+        // this way we can control the access to the data members of the class
+        // we can also modify the setData() method to accept parameters to set different values for different objects
     }
 }
