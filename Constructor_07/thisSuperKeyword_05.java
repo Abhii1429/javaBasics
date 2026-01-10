594a31e7be4999114828da89528660eddcfb0eb0
Abhishek <hopesaliveabhi2003@gmail.com>
2026-01-10 20:01:09 +0530

    this v/s super keyword

--- Constructor_07/thisSuperKeyword_05.java
+++ Constructor_07/thisSuperKeyword_05.java
@@ -8,24 +8,26 @@ class Student1{
        // super(); // Calling parent class constructor
         // Implicitly added if not written
 
-        this("Rohit", 19); //[2] // 3) now control goes to Parameterized Constructor with two parameters because this() contains two parameters
+        this("Rohit", 19); //[2] // 3) now control goes to Parameterized Constructor with 
+                                           // two parameters because this() contains two parameters
 
         System.out.println("Default Constructor is called");
         name = "Rohan";
         age = 18;
-    } // 6) after this constructor ends control goes back to main method where it had left off
-    
+    }                  // 6) after this constructor ends control goes back to main method where it had left off
+
     public Student1(String name){ // 9) control comes here from main method and values are initialized here name="Rahul"
 
-        this(); //[1] // from here control goes to zero parameter constructor
+        this(); //[1]    // from here control goes to default constructor because this() has no parameters
         this.name = name;
         age = 19;
-    } // 10) after this constructor ends, control goes back to main method where it had left off
+    }            // 10) after this constructor ends, control goes back to main method where it had left off
     
     public Student1(String name, int age){ // 4) control comes here from this("Rohit",19) and initializes the values
         this.name = name;
         this.age = age;
-    } // 5) after this constructor ends, control goes back to default constructor where it had left off
+    }          // 5) after this constructor ends, control goes back to default constructor where it had left off
+    
     
     public void disp(){
         System.out.println(name);
@@ -35,7 +37,7 @@ class Student1{
 // [1] & [2] are called constructor chaining.
 public class thisSuperKeyword_05 {
     public static void main(String[] args){
-        Student1 st1 = new Student1(); // 1)control come from main to this and then it goes to default constructor outside main method
+        Student1 st1 = new Student1(); // 1)control come from main() to this and then it goes to default constructor outside main method
         st1.disp(); // 7) after all constructors are executed control comes back to main method and disp() is called
 
         Student1 st2 = new Student1("Rahul"); // 8) now control from here goes to parameterized constructor with one parameter
---
+++ Inheritance_08/inheritance_01.java
@@ -0,0 +1,17 @@
+package Inheritance_08;
+
+class Human{
+    int age;
+    void sleep(){
+        age = 18;
+        System.out.println("Human needs good sleep");
+    }
+}
+
+class Student{
+
+}
+
+public class inheritance_01 {
+    
+}
