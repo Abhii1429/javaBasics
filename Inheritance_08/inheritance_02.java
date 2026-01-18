d242d2c2f7b1c434e1b2e7fc7c34f9ede004ef02
Abhishek <hopesaliveabhi2003@gmail.com>
2026-01-18 19:16:23 +0530

    Properties of inheritance(private,constructor)

--- Inheritance_08/inheritance_02.java
+++ Inheritance_08/inheritance_02.java
@@ -1,12 +1,12 @@
 package Inheritance_08;
 
-class Human1{
+class Human1{ // 4.) control from (3) comes to the parent class; it searches for zero parameter constructor and finds it.
     private String name;
     int age;
 
-    Human1(){ // constructor class
+    Human1(){ // constructor class  // 5.) control comes here and executes the constructor body
         System.out.println("Human class constructor");
-    }
+    } // after body execution, control goes back to (2) and then to (1)
 
     void sleep(){
         age = 18;
@@ -15,11 +15,12 @@ class Human1{
     }
 }
 
-class Student1 extends Human1{
+class Student1 extends Human1{ // Human1 is parent class of Student1 class
 
-    // public Student1(){ // default Student1() constructor automatically provided by the java compiler,
-                // super();  // it calls the parent class constructor Human1()
-                             // it is automatically inserted by the java compiler if we don't write it explicitly.   
+    // public Student1()
+    // {                     // 2.) behind the scene, default Student1() constructor provided by the java compiler .
+         // super();         // 3.) it calls the parent class constructor, Human1()
+                        // super() method call is automatically inserted by the java compiler if we don't write it explicitly.   
     // }
     void disp(){ // method
         System.out.println("The age is: "+age); // age is not part of this method but still we are able to use it, as it's part of child class.
@@ -32,8 +33,9 @@ class Student1 extends Human1{
 
 public class inheritance_02 {
     public static void main(String[] args){
-        Student1 st1 = new Student1(); // during object creation "Student1()" constructor is called, but there is no contructor in that  student class.
+        Student1 st1 = new Student1(); //1.) during object creation "Student1()" constructor is called, but there is no contructor in that  student class.
                                        // as it is automatically provided by the java compiler, which internally calls the parent class constructor "Human1()"  
+                                      
                                        // hence the output shows the constructor members also.
                                        // this is the reason why constructor don't participate in inheritance.
                                        // because even if there is no constructor in the child class, the parent class constructor is still called.
@@ -49,6 +51,7 @@ public class inheritance_02 {
 }
 
 // when constructor don't participate in inheritance then why output shows the constructor members?
+//Ans=> 1-3
 
 // output:
 // Human class constructor ??
