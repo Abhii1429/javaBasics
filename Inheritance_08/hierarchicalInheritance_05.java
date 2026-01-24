f7211165c24d8aa1a3a72a52d1cb036ee04bb68f
Abhishek <hopesaliveabhi2003@gmail.com>
2026-01-24 12:40:58 +0530

    hierarchical Inheritance

---
+++ Inheritance_08/hierarchicalInheritance_05.java
@@ -0,0 +1,29 @@
+package Inheritance_08;
+// Hierarchical Inheritance
+class Animal // extends Objects
+{            // all the properties of Object class are inherited to Animal class
+    void disp(){
+        System.out.println("Animal needs sleep");
+    }
+}
+
+class Tiger extends Animal // extends Objects
+                           // all the properties of Object class are inherited to Animal class
+{
+
+}
+class Monkey extends Animal
+{
+
+}
+class Deer extends Animal
+{
+
+}
+
+public class hierarchicalInheritance_05 {
+    public static void main(String[] args){
+        Deer d = new Deer();
+        d.disp(); 
+    }
+}
