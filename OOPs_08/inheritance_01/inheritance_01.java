b8407712ad7579a9abbe80c483d244dcc6f44e7c
Abhishek <hopesaliveabhi2003@gmail.com>
2026-01-31 21:01:03 +0530

    basics of inheritance

---
+++ OOPs_08/inheritance_01/inheritance_01.java
@@ -0,0 +1,25 @@
+package OOPs_08.inheritance_01;
+class Human{ // super class / parent class / base class
+    int age;
+    void sleep(){
+        age = 18;
+        System.out.println("Human needs good sleep");
+        System.out.println(age);
+    }
+}
+
+class Student extends Human // in java; this is also known as "is a" relationship
+{
+    // Student class => sub class / child class / derived class
+    // "extends" keyword facilitates the "relationship" between the classes(inheritance) 
+  // this class contains the properties of Human class(Parent class)
+}
+
+public class inheritance_01 {
+    public static void main(String[] args){
+        Student st = new Student();
+
+        st.sleep();
+    }
+}
+
